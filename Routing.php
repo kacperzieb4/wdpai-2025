6ac0d15d688a83ac2f5831334e6e88d6cf5b7729
Kacper Zięba <167540203+kacperzieb4@users.noreply.github.com>
2025-10-27 14:04:55 +0100

    login and csses

--- Routing.php
+++ Routing.php
@@ -1,15 +1,35 @@
 <?php
 
-class Routing{
+require_once 'src/controllers/SecurityController.php';
+
+class Routing {
+
+    public static $routes = [
+        "login" => [
+            "controller" => "SecurityController",
+            "action" => "login"
+        ],
+        "register" => [
+            "controller" => "SecurityController",
+            "action" => "register"
+        ]
+
+        ];
+
     public static function run(string $path){
         switch($path){
             case 'dashboard':
+
                 include 'public/views/dashboard.html';
                 echo "<h2>Dashboard</h2>";
                 break;
             case 'login':
-                include 'public/views/login.html';
-                echo "<h2>Login</h2>";
+            case 'register':
+                $controller = Routing::$routes[$path]["controller"];
+                $action = Routing::$routes[$path]["action"];
+
+                $controllerObj = new $controller();
+                $controllerObj->$action();
                 break;
             default:
                 include 'public/views/404.html';
